deb063dc95c32034a7d718452da82830819c8b33
CWF105 <cw.flaviano105@gmail.com>
2025-02-10 14:12:54 +0800

    camera fixed
    - camera orientation fixed.
    - camera feed fixed.

--- Assets/Packages/MediaPipeUnity/Tutorial/Official Solution/FaceMesh.cs
+++ Assets/Packages/MediaPipeUnity/Tutorial/Official Solution/FaceMesh.cs
@@ -10,7 +10,6 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 namespace Mediapipe.Unity.Tutorial
 {
   public class FaceMesh : MonoBehaviour
@@ -22,20 +21,34 @@ namespace Mediapipe.Unity.Tutorial
 
     private WebCamTexture _webCamTexture;
 
-    private IEnumerator Start()
-    {
-      if (WebCamTexture.devices.Length == 0)
-      {
-        throw new System.Exception("Web Camera devices are not found");
-      }
-      var webCamDevice = WebCamTexture.devices[0];
-      _webCamTexture = new WebCamTexture(webCamDevice.name, _width, _height, _fps);
-      _webCamTexture.Play();
+      private IEnumerator Start()
+      { 
+        if (WebCamTexture.devices.Length == 0)
+        {
+            throw new System.Exception("Web Camera devices are not found");
+        }
 
-      yield return new WaitUntil(() => _webCamTexture.width > 16);
+        var webCamDevice = WebCamTexture.devices[0];
+        _webCamTexture = new WebCamTexture(webCamDevice.name, _width, _height, _fps);
+        _webCamTexture.Play();
 
-      _screen.rectTransform.sizeDelta = new Vector2(_width, _height);
-      _screen.texture = _webCamTexture;
+        // Wait until the camera starts properly
+        yield return new WaitUntil(() => _webCamTexture.width > 16);
+
+        // Change screen color to white
+        _screen.color = UnityEngine.Color.white;
+
+        // Ensure the camera output is rotated to vertical
+        float aspectRatio = (float)_webCamTexture.width / _webCamTexture.height;
+        
+        // Swap width & height to make it vertical
+        _screen.rectTransform.sizeDelta = new Vector2(_height, _height / aspectRatio);
+
+        // Rotate the camera feed by 90 degrees to make it vertical
+        _screen.rectTransform.localEulerAngles = new Vector3(0, 0, -90);
+
+        // Apply the webcam texture
+        _screen.texture = _webCamTexture;
     }
 
     private void OnDestroy()
